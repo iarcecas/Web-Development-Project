bec790ba64dbd3d14ff262fb4b3c1dce68074318
iarcecas <114025089+iarcecas@users.noreply.github.com>
2023-11-08 13:01:48 -0500

    Restart

--- models/category.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const CategorySchema = new mongoose.Schema({
-    name: String
-});
-
-const Category = mongoose.model('Caterogy', CategorySchema);
-
-module.exports = Category;
